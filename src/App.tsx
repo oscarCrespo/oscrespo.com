685ff07c2a08afa286b137e15aa66b3e4ab08b12
OscarCrespo <{ID}+{username}@users.noreply.github.com>
2018-11-25 03:35:03 -0500

    Completed Home section adn added right theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./Home";
+// import cli from "./Cli";
 
-class App extends Component {
+export default class App extends Component {
+  // componentDidMount() {
+  //   (window as any).cli = cli;
+  //   console.log("Hello There, type cli to start...");
+  // }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Home />
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/Cli.ts
@@ -0,0 +1,4 @@
+const cli = () => {
+  console.log("Hello");
+};
+export default cli;
